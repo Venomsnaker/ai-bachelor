6a5c35af54711cc0abc7d08a110aeb1fe75a72a9
venomsnaker <qhuy.bui.developer@gmail.com>
2024-10-24 16:17:44 +0700

    Update Other Benchmarks - Selfcheck GPT

--- selfcheck-gpt/modeling_selfcheck.py
+++ selfcheck-gpt/modeling_selfcheck.py
@@ -10,7 +10,7 @@ logging.set_verbosity_error()
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, AutoModelForCausalLM
 from transformers import LongformerTokenizer, LongformerForMultipleChoice, LongformerForSequenceClassification
 from transformers import DebertaV2ForSequenceClassification, DebertaV2Tokenizer
-from selfcheckgpt.utils import MQAGConfig, expand_list1, expand_list2, NLIConfig, LLMPromptConfig
+from utils import MQAGConfig, expand_list1, expand_list2, NLIConfig
 from modeling_mqag import question_generation_sentence_level, answering
 from modeling_ngram import UnigramModel, NgramModel
 from modeling_selfcheck_apiprompt import SelfCheckAPIPrompt
